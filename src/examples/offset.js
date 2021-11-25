c56235f03cfc1f2c4d1867fc18251e8d10e91449
Joshua Weinberg <josh.weinberg@gmail.com>
2021-11-25 11:34:27 -0700

    wiki

--- src/examples/offset.js
+++ src/examples/offset.js
@@ -5,12 +5,45 @@ import {table} from "../output.js";
 import {initJSDB} from "../index.js";
 import {setupDatabase} from "./sampleData.js";
 import {FROM} from "../from.js";
+import {CREATE_TABLE} from "../createTable.js";
+import {INSERT_INTO} from "../insertInto.js";
+import {LIMIT} from "../limit.js";
 
 initJSDB();
 setupDatabase();
 let employee;
-let result;
 
+CREATE_TABLE('filenames');
+INSERT_INTO('filenames', [
+  {filename:'00-INDEX'},
+  {filename:'07'},
+  {filename:'1040.bin.ihex'},
+  {filename:'11d.c'},
+  {filename:'11d.h'},
+  {filename:'1200.bin.ihex'},
+  {filename:'12160.bin.ihex'},
+  {filename:'1232ea962bbaf0e909365f4964f6cceb2ba8ce'},
+  {filename:'1280.bin.ihex'},
+  {filename:'15562512ca6cf14c1b8f08e09d5907118deaf0'},
+  {filename:'17'},
+  {filename:'1d'},
+  {filename:'1.Intro'},
+  {filename:'21142.c'},
+  {filename:'21285.c'},
+  {filename:'2860_main_dev.c'},
+  {filename:'2860_rtmp_init.c'},
+  {filename:'2870_main_dev.c'},
+  {filename:'2870_rtmp_init.c'},
+  {filename:'2.Process'},
+  {filename:''},
+]);
+const filenames = FROM('filenames');
+let result = OFFSET(filenames, 10); // 0 offset is just for example
+result = LIMIT(result, 10);
+table(result);
+process.exit();
+
+/*
 employee = FROM('employee');
 result = SELECT(employee, ['name', 'status', 'salary']);
 result = ORDER_BY(result, (a,b) => a.salary - b.salary);
--- src/examples/orderBy.js
+++ src/examples/orderBy.js
@@ -5,17 +5,30 @@ import {ORDER_BY} from "../orderBy.js";
 import {FROM} from "../from.js";
 import {initJSDB} from "../index.js";
 import {setupDatabase} from "./sampleData.js";
+import {INSERT_INTO} from "../insertInto.js";
+import {DROP_TABLE} from "../dropTable.js";
+import {CREATE_TABLE} from "../createTable.js";
 
 initJSDB();
 setupDatabase();
 let employee;
 let result;
 
+DROP_TABLE('employee');
+CREATE_TABLE('employee');
+INSERT_INTO('employee',[
+  {id:1,name:'Josh',department_id:1,role:'Worker'},
+  {id:2,name:'Ruth',department_id:2,role:'Worker'},
+  {id:3,name:'Jake',department_id:1,role:'Worker'},
+  {id:4,name:'John',department_id:2,role:'Worker'},
+  {id:5,name:'Alice',department_id:2,role:'Manager'},
+  {id:6,name:'Dan',department_id:1,role:'Manager'},
+  {id:7,name:'Janet',department_id:1,role:'Manager'},
+]);
 employee = FROM('employee');
-result = SELECT(employee, ['name', 'status']);
-result = ORDER_BY(result, (a,b) => { if (a.name < b.name) { return -1; } else { return 1; } });
-console.log('Order by name:');
+result = ORDER_BY(employee, (a, b) => a['name'].localeCompare(b['name']));
 table(result);
+process.exit();
 
 /*
 ┌───────────┬────────────┐
